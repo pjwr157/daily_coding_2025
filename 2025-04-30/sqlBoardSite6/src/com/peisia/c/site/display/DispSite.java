3c4d63203bc3126446ef7e263117c8df377b23e8
pjwr157 <pjwr157@gmail.com>
2025-04-30 17:36:36 +0900

    2025-04-30 학습 내용 추가 및 연휴 시작(MySQL & Java)

---
+++ 2025-04-30/sqlBoardSite6/src/com/peisia/c/site/display/DispSite.java
@@ -0,0 +1,19 @@
+package com.peisia.c.site.display;
+
+import com.peisia.c.util.Cw;
+
+public class DispSite {
+	static private String SITE_NAME = "Peisia";
+	static private String VERSION = " V 6";
+	static public void entranceTitle() {
+		Cw.line();
+		Cw.dot();
+		Cw.space(22);
+		Cw.w(SITE_NAME);
+		Cw.w(VERSION);
+		Cw.space(22);
+		Cw.dot();
+		Cw.wn();
+		Cw.line();
+	}	
+}
